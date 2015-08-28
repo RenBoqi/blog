7467f48048d5e11e1382ce131abdad3f37daeefa
DINGKAI <DINGKAI@renboqi>
2015-08-28 10:26:32 +0200

    1.0.2 Vision

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+console.log("this ist for js")
+
+var speed = 3000 
+var img= new Array()
+var current = 0
+
+img[0] = new Image(); img[0].src = "images/1.jpg";	
+img[1] = new Image(); img[1].src = "images/2.jpg";
+img[2] = new Image(); img[2].src = "images/3.jpg";
+
+
+
+function start() 
+{
+current++
+if(current >=img.length) current = 0
+document.imgs.src = img[current].src
+setTimeout("start()",speed);
+}
+
